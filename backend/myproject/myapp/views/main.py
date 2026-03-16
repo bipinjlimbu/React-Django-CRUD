afc9920b74ec59b52962310174c7a9e1cf9c9c77
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-16 18:38:34 +0545

    Student POST View Created

---
+++ backend/myproject/myapp/views/main.py
@@ -0,0 +1,15 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from ..models import Student
+from ..serializers import StudentSerializer
+from rest_framework import status
+
+@api_view(['POST'])
+def student_view(request):
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Student created successfully','data': serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Failed to create student','errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
