5a9dd2dd747c26206d1fa19d5a330c466b8a0cfa
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-16 12:56:42 +0545

    Tailwind CSS Integrated

--- frontend/my-project/src/pages/Index.jsx
+++ frontend/my-project/src/pages/Index.jsx
@@ -1,8 +1,12 @@
 const Index = () => {
     return (
-        <div>
-            <h1>Welcome to the Index Page</h1>
-        </div>
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="text-4xl font-bold text-blue-500">
+                    Tailwind v3 Working 🚀
+                </h1>
+            </div>
+        </>
     );
 }
 
