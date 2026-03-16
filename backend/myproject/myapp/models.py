b55fe6f071ff480595179653c5ad84a4ffbac194
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-16 18:33:53 +0545

    Student Model Created

--- backend/myproject/myapp/models.py
+++ backend/myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll = models.IntegerField(unique=True)
+    faculty = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
