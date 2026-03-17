3cd00dd88c29a2fe120e867b94dc41e80c0af9b3
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-17 12:39:49 +0545

    Student Detail View Url Added

--- backend/myproject/myapp/urls.py
+++ backend/myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views.main import student_view
+from .views.main import student_view, student_detail_view
 
 urlpatterns = [
     path('students/', student_view, name='student_view'),
+    path('students/<int:student_id>/', student_detail_view, name='student_detail_view'),
 ]
